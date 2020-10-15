5a8388e179d7b0f9dc5f74f75330b259e78e628e
Batchen <30310590+BatchenIt@users.noreply.github.com>
2020-10-15 15:26:22 +0300

    Angular material updated

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule, } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
